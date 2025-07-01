9ddfb541920f69c933729542700e6c5ed2d78c93
hp <hp@LAPTOP-14G6ACJ1>
2025-07-01 18:25:56 +0530

    first

---
+++ BMMBlastFurnace/src/test/java/carrier/Launch.java
@@ -0,0 +1,20 @@
+package carrier;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Launch {
+	@Test
+	public void login()
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://jobs.carrier.com/en");
+		
+		
+	}
+}
