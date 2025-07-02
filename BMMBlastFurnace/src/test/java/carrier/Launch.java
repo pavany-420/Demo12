216c0dcdfd965898812a6fdbdf21e355a82ba0d5
hp <hp@LAPTOP-14G6ACJ1>
2025-07-02 10:04:59 +0530

    updated Reporter.log

--- BMMBlastFurnace/src/test/java/carrier/Launch.java
+++ BMMBlastFurnace/src/test/java/carrier/Launch.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Launch {
